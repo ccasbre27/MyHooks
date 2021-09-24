4e7e7f073683842b9dd56d391ea1fda5c7334e0f
SOUTHAMERICA\carloscastro <carloscastro@microsoft.com>
2021-09-24 17:03:08 -0600

    add hooks

---
+++ useFetch.js
@@ -0,0 +1,47 @@
+import { useEffect, useState, useRef } from "react";
+
+export const useFetch = ( url ) => {
+    
+    const isMounted = useRef(true);
+    const [state, setState] = useState({ data: null, loading: true, error: null });
+
+    useEffect(() => {
+        return () => {
+            isMounted.current = false;
+        }
+    }, [])
+
+    useEffect(() => {
+
+        setState({
+            loading: true,
+            error: null,
+            data: null
+        });
+        
+        fetch( url )
+            .then( resp => resp.json() )
+            .then( data => {
+
+                if ( isMounted.current ) {
+                    setState({
+                        loading: false,
+                        error: null,
+                        data: data
+                    });
+                }
+            })
+            .catch( () => {
+
+                setState({
+                    loading: false,
+                    error: 'Could not load info',
+                    data: null
+                });
+
+            });
+        
+    }, [url])
+
+    return state;
+}
